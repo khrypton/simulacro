99efb7f5f2dbcf341c879b52256d595f923fd832
Khrypton <143830@unsaac.edu.pe>
2023-10-13 22:10:31 -0500

    Esto es la primera modificacion de la rama John

---
+++ lib/screens/john_sreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class JohnWidget extends StatelessWidget {
+  const JohnWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hola es es un demos"),
+      ),
+    );
+  }
+}
